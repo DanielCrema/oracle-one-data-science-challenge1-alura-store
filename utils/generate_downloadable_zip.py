e35116ef01605027b383abbf7726d232f91c84f1
DanielCrema <danielborgescrema@gmail.com>
2025-04-22 15:56:01 -0300

    fix csv indexing

--- utils/generate_downloadable_zip.py
+++ utils/generate_downloadable_zip.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 import os
 import zipfile
 import io
@@ -20,7 +19,7 @@ def zip_files(lojas_comparisons: dict) -> bytes:
     lojas_sales_distribution = lojas_comparisons['lojas_sales_distribution']
 
     def convert_df_to_csv(df):
-        return df.to_csv(index=False).encode('utf-8')
+        return df.to_csv(index=True).encode('utf-8')
 
     # Create a zip file in memory
     zip_buffer = io.BytesIO()
@@ -38,7 +37,7 @@ def zip_files(lojas_comparisons: dict) -> bytes:
         zip_file.writestr("estatisticas_globais.csv", convert_df_to_csv(lojas_stats.T))
         zip_file.writestr("categorias_mais_vendidas.csv", convert_df_to_csv(lojas_categories))
         zip_file.writestr("produtos_mais_vendidos.csv", convert_df_to_csv(lojas_products))
-        zip_file.writestr("frete_medio_por_produto.csv", convert_df_to_csv(lojas_top10_products_shipping_mean))
+        zip_file.writestr("frete_medio_top10_produtos.csv", convert_df_to_csv(lojas_top10_products_shipping_mean))
         zip_file.writestr("distribuicao_geografica_vendas.csv", convert_df_to_csv(lojas_sales_distribution))
 
     # Move back to the start of the BytesIO buffer
